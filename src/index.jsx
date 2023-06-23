19c0b21c2107894f94519a21a49024a9a9d46c68
joaquingutierrez <joaquinguty@gmail.com>
2023-06-23 15:32:54 -0300

    Inicializando con la app

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+import { styles } from "./style"
+
+export default function App() {
+
+    return (
+        <View style={styles.container}>
+            <Text>Control de stock</Text>
+        </View>
+    );
+}
