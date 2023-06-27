28cee586fa74a85c7b700399010ae1b460e5e7e0
joaquingutierrez <joaquinguty@gmail.com>
2023-06-27 20:19:18 -0300

    Implementacion: Ahora se pueden agregar productos al carrito manualmente utilizando el boton Agregar al carrito

--- src/screens/ProductDetailScreen/index.jsx
+++ src/screens/ProductDetailScreen/index.jsx
@@ -1,24 +1,67 @@
-import { View } from "react-native";
-import { useSelector } from "react-redux";
+import { View, Modal, Text, Button } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
 import { styles } from "./style"
-import { ItemDetail } from "../../components";
+import { InputNumber, ItemDetail } from "../../components";
 import CustomText from "../../components/CustomText";
+import { addToCart } from "../../store/reducers/cartSlice";
 
 const ProductDetailScreen = ({ route }) => {
-
+    
+    const dispatch = useDispatch()
     const products = useSelector(state => state.product.data)
+    const [modalVisible, setModalVisible] = useState(false);
+    const [number, setNumber] = useState(0)
 
     const product = products.find(product => product.id === route.params.id)
 
     const handleAddToCart = () => {
-        console.log("Agregar al carrito")
+        setModalVisible(true)
+    }
+    const dispatchAddToCart = () => {
+        const payload = {
+            item: product,
+            quantity: number
+        }
+        dispatch(addToCart(payload))
+        setModalVisible(false)
+    }
+
+    const handleNumberSubstract = () => {
+        const updateNumber = number - 1
+        setNumber(updateNumber)
+    }
+    const handleNumberAdd = () => {
+        const updateNumber = number + 1
+        setNumber(updateNumber)
     }
 
     return (
         <View style={styles.container}>
             {product ? (
-                <ItemDetail item={product} handleAddToCart={handleAddToCart} />
+                <>
+                    <ItemDetail item={product} handleAddToCart={handleAddToCart} />
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={modalVisible}
+                        onRequestClose={() => {
+                            Alert.alert('Modal has been closed.');
+                            setModalVisible(!modalVisible);
+                        }}>
+                        <View style={styles.centeredView}>
+                            <View style={styles.modalView}>
+                                <Text style={styles.modalText}>Cantidad a agregar:</Text>
+                                <InputNumber number={number} handleNumberSubstract={handleNumberSubstract} handleNumberAdd={handleNumberAdd} />
+                                <View>
+                                    <Button title="Aceptar" onPress={dispatchAddToCart}/>
+                                    <Button title="Cancelar" onPress={()=>setModalVisible(false)} />
+                                </View>
+                            </View>
+                        </View>
+                    </Modal>
+                </>
             ) : (
                 <CustomText myCustomText="Producto no encotrado" />
             )}
